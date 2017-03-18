b993efb093509e9e6793457009492d2a13d6cb4d
AleksiEnsioLaine <aleksilaine91@gmail.com>
2017-03-18 19:36:06 +0200

    everything in order

---
+++ src/library/Book.java
@@ -0,0 +1,14 @@
+package library;
+
+public class Book {
+	final int MAXIMUM_PAGES = 3000;
+	public Book() {
+		int pages, yearPublished;
+		String title, author;
+	}
+	public void setPages(int p){
+		pages = p;
+		System.out.println("The Book pages set to " + p);
+	}
+	
+}
---
+++ src/library/MainClass.java
@@ -0,0 +1,16 @@
+package library;
+
+/*
+ * Main class of the library project
+ * @author Aleksi Laine
+ * 
+ */
+public class MainClass {
+
+	public static void main(String[] args) {
+		
+		Book warAndPeace = new Book();
+		warAndPeace.setPages(1000);
+	}
+
+}
