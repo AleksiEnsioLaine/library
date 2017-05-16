416d5bf98eef55d5c40168b41d27f6e4144e94ff
aleksiensiolaine <aleksilaine91@gmail.com>
2017-05-16 11:55:28 +0300

    Loan class added

--- src/library/Loan.java
+++ src/library/Loan.java
@@ -1,9 +1,17 @@
 package library;
 
+import java.util.Date;
+
 public class Loan {
 
-	public Loan() {
-		
+	Customer customer;
+	Book book;
+	Date loanStartDate;
+	
+	public Loan(Customer cust, Book bookToLoan) {
+	customer = cust;
+	book = bookToLoan;
+	loanStartDate =  new Date();
 	}
 
 }
